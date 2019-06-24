75b96bf1fe63fa7b3d74d873b57eb5ea0f1b8cd8
Maxwell Dantas <maxwelldsouza@hotmail.com>
2019-06-24 16:46:08 -0300

    Atualização de configuração da API Key do SendGrid para envio de e-mails

--- src/main/java/br/com/ongvd/service/EmailServiceImpl.java
+++ src/main/java/br/com/ongvd/service/EmailServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sendgrid.Content;
@@ -15,19 +14,14 @@ import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 
+import br.com.ongvdsendgrid.OngvdSendgridApplication;
+
 @Service
 public class EmailServiceImpl implements EmailService {
 	
 	private final Logger LOG = LoggerFactory.getLogger(EmailServiceImpl.class);
 	
-	private SendGrid sendGridClient;
-	
-    @Autowired
-    public EmailServiceImpl(SendGrid sendGridClient) {
-        this.sendGridClient = sendGridClient;
-    }
-    
-    @Override
+	@Override
     public void enviarTexto(String emailDe, String emailPara, String assunto, String corpo) {
         Response response = enviarEmail(emailDe, emailPara, assunto, new Content("text/plain", corpo));
         LOG.info("Status Code: " + response.getStatusCode() + ", Body: " + response.getBody() + ", Headers: "
@@ -44,13 +38,14 @@ public class EmailServiceImpl implements EmailService {
     private Response enviarEmail(String emailDe, String emailPara, String assunto, Content content) {
         Mail mail = new Mail(new Email(emailDe), assunto, new Email(emailPara), content);
         mail.setReplyTo(new Email(emailDe));
+        SendGrid sg = new SendGrid(OngvdSendgridApplication.SENDGRI_API_KEY);
         Request request = new Request();
         Response response = null;
         try {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
-            response = sendGridClient.api(request);
+            response = sg.api(request);
         } catch (IOException ex) {
         	LOG.info(ex.getMessage());
         }
